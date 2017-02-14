a76686e163de963002a8b1231bba4f559ce2079b
razaaan <Zibyan95@gmail.com>
2017-02-15 01:27:34 +0300

    Basic pen works

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -28,12 +28,88 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pnlDraw = new System.Windows.Forms.Panel();
+			this.btnPenColor = new System.Windows.Forms.Button();
+			this.barPenWidth = new System.Windows.Forms.TrackBar();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.barPenWidth)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pnlDraw
+			// 
+			this.pnlDraw.BackColor = System.Drawing.Color.White;
+			this.pnlDraw.Location = new System.Drawing.Point(198, 5);
+			this.pnlDraw.Name = "pnlDraw";
+			this.pnlDraw.Size = new System.Drawing.Size(552, 436);
+			this.pnlDraw.TabIndex = 0;
+			this.pnlDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseDown);
+			this.pnlDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseMove);
+			this.pnlDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlDraw_MouseUp);
+			// 
+			// btnPenColor
+			// 
+			this.btnPenColor.Location = new System.Drawing.Point(6, 122);
+			this.btnPenColor.Name = "btnPenColor";
+			this.btnPenColor.Size = new System.Drawing.Size(162, 32);
+			this.btnPenColor.TabIndex = 1;
+			this.btnPenColor.Text = "Pen Color";
+			this.btnPenColor.UseVisualStyleBackColor = true;
+			this.btnPenColor.Click += new System.EventHandler(this.btnPenColor_Click);
+			// 
+			// barPenWidth
+			// 
+			this.barPenWidth.Location = new System.Drawing.Point(6, 46);
+			this.barPenWidth.Maximum = 20;
+			this.barPenWidth.Name = "barPenWidth";
+			this.barPenWidth.Size = new System.Drawing.Size(162, 56);
+			this.barPenWidth.TabIndex = 2;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.btnPenColor);
+			this.groupBox1.Controls.Add(this.barPenWidth);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(180, 180);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Pen Control";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(73, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Pen Width";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(761, 454);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.pnlDraw);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.barPenWidth)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel pnlDraw;
+		private System.Windows.Forms.Button btnPenColor;
+		private System.Windows.Forms.TrackBar barPenWidth;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -12,9 +12,64 @@ namespace Paint
 {
 	public partial class Form1 : Form
 	{
+		Graphics g;
+		bool startPaint = false;
+
+		int? coordX = null;
+		int? coordY = null;
+
+		
 		public Form1()
 		{
 			InitializeComponent();
+			g = pnlDraw.CreateGraphics();
+			
+		}
+
+		//opens color picker 
+		private void btnPenColor_Click(object sender, EventArgs e)
+		{
+			ColorDialog c = new ColorDialog();
+			if (c.ShowDialog() == DialogResult.OK)
+			{
+				btnPenColor.BackColor = c.Color;
+			}
+		}
+
+		private void pnlDraw_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (startPaint)
+			{
+				//set pen to options
+				Pen pen = new Pen(btnPenColor.BackColor, barPenWidth.Value);
+
+				g.DrawLine(pen, new Point(coordX ?? e.X, coordY ?? e.Y), new Point(e.X, e.Y));
+				/* ?? returns left if not null, else returns right
+				 * coordX ?? e.X equivalent to:
+				 * if (coordX != null)
+				 *    use e.X
+				 * else
+				 *    use coordX
+				 *    
+				 */
+
+
+				coordX = e.X;
+				coordY = e.Y;
+			}
+		}
+
+		private void pnlDraw_MouseDown(object sender, MouseEventArgs e)
+		{
+			startPaint = true; //allows pnlDraw_MouseMove to work
+		}
+
+		private void pnlDraw_MouseUp(object sender, MouseEventArgs e)
+		{
+			startPaint = false;
+
+			coordX = null;
+			coordY = null;
 		}
 	}
 }
